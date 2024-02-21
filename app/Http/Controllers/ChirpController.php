3cf847d508a317a7b437c9cee3491ebe19da1978
NafiurSiddiqui <saejim91@gmail.com>
2024-02-21 23:56:33 +0600

    db: post stores & frontend updated

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -15,7 +16,7 @@ class ChirpController extends Controller
     public function index(): Response
     {
         return Inertia::render('Chirps/Index',[
-            //
+            'chirps'=> Chirp::with('user:id,name')->latest()->get(),
         ]);
     }
 
@@ -30,9 +31,17 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        //validate
+        $validated = $request->validate([
+            'message' => 'required|string|max:255'
+        ]);
+
+        //store
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
